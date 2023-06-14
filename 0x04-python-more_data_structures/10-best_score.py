f52c371df4ff0c7d174e0b0bf1fbb27acf6ea616
elhanabullahi <elhanabullahi@gmail.com>
2023-06-14 12:11:42 +0300

    def best_score(a_dictionary)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_d = {}
-    for i in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-    return new_d
+
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
